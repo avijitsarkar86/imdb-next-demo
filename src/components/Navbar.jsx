6c1fce45e5842bf092ff03bfce9bf81999d18785
Avijit Sarkar <avijit.a.sarkar@capgemini.com>
2024-06-04 23:03:18 +0530

    added suspense

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import NavbarItem from "./NavbarItem"
 
 export default function Navbar() {
   return (
     <div
       className="flex dark:bg-gray-600 bg-amber-100 p-4 
-      lg:text-lg justify-center gap-6">
-      <NavbarItem title="Trending" param="fetchTrending" />
-      <NavbarItem title="Top Rated" param="fetchTopRated" />
+      lg:text-lg justify-center gap-6"
+    >
+      <Suspense fallback={<div>Loading...</div>}>
+        <NavbarItem title="Trending" param="fetchTrending" />
+        <NavbarItem title="Top Rated" param="fetchTopRated" />
+      </Suspense>
     </div>
   )
 }
